91a50ba06e901c8090d16155cc334adf81232748
Switchu <remplak@gmail.com>
2025-10-01 18:01:40 +0200

    Update MoveAbilityAction.cs

--- Assets/_SunsetSystems/Action System/Ability Actions/MoveAbilityAction.cs
+++ Assets/_SunsetSystems/Action System/Ability Actions/MoveAbilityAction.cs
@@ -39,7 +39,6 @@ namespace SunsetSystems.ActionSystem
 
         public override void Begin()
         {
-            navigationManager.SetNavigationTarget(destination);
             bool destinationSet = navigationManager.SetNavigationTarget(destination);
             if (!destinationSet)
             {
