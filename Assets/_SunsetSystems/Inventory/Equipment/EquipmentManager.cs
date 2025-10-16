f88a63ad7c3f4913be8622afcae91d46dc63cbdf
Switchu <remplak@gmail.com>
2025-10-16 17:30:32 +0200

    Update EquipmentManager.cs

--- Assets/_SunsetSystems/Inventory/Equipment/EquipmentManager.cs
+++ Assets/_SunsetSystems/Inventory/Equipment/EquipmentManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using NPOI.SS.Formula.Functions;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using SunsetSystems.Core.Database;
